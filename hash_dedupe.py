63f30543c62a70c2b280546d7c8d8b711d1b52c3
Asilvajr <asilvajr@ymail.com>
2024-07-23 01:04:51 -0500

    Init hash_dedupe.py

---
+++ hash_dedupe.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Build a Hashtable using a python dict. 
+# Dict(Key => Value) => Dict(Sha256-Hash => List[(Filenames, Size,Date, Quality)])
+# Output to file the results
+
+
+
+
